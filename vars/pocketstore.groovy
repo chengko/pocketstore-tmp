f1fafaad78afc93e9ec3dc39898ea8c814692b55
cheng <cheng@7senses.com>
2024-01-22 18:44:30 +0800

    try

--- vars/pocketstore.groovy
+++ vars/pocketstore.groovy
@@ -1,31 +1,32 @@
 def configureBuild(String projectName, String buildSetting, String privateSite) {
     stage('Configure') {
-        agent { 
-            label 'unityci' 
-        }
+        
         script {
-            if (buildSetting == '(Private)') {
-                
-                def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
-                
-                BUILD_SETTINGS = "${destDir}/${privateSite}.json"
+            node('unityci') {
+                if (buildSetting == '(Private)') {
+                    
+                    def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
+                    
+                    BUILD_SETTINGS = "${destDir}/${privateSite}.json"
 
-                sh "mkdir -p ${destDir}"
+                    sh "mkdir -p ${destDir}"
 
-                echo destDir
+                    echo destDir
 
-                echo BUILD_SETTINGS
+                    echo BUILD_SETTINGS
 
-                def content = """
-                {
-                    "SkipWebConfig": false,
-                    "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
-                    "LocalSite": "Private_${privateSite}"
+                    def content = """
+                    {
+                        "SkipWebConfig": false,
+                        "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
+                        "LocalSite": "Private_${privateSite}"
+                    }
+                    """
+                    
+                    writeFile file: BUILD_SETTINGS, text: content
                 }
-                """
-                
-                writeFile file: BUILD_SETTINGS, text: content
             }
+            
         }
         
     }
