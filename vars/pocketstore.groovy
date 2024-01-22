44fafc5f161ea67fa04ad7146639bc0369e3f909
cheng <cheng@7senses.com>
2024-01-22 18:45:40 +0800

    try

--- vars/pocketstore.groovy
+++ vars/pocketstore.groovy
@@ -1,33 +1,27 @@
 def configureBuild(String projectName, String buildSetting, String privateSite) {
-    stage('Configure') {
-        
-        script {
-            node('unityci') {
-                if (buildSetting == '(Private)') {
-                    
-                    def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
-                    
-                    BUILD_SETTINGS = "${destDir}/${privateSite}.json"
-
-                    sh "mkdir -p ${destDir}"
-
-                    echo destDir
-
-                    echo BUILD_SETTINGS
-
-                    def content = """
-                    {
-                        "SkipWebConfig": false,
-                        "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
-                        "LocalSite": "Private_${privateSite}"
-                    }
-                    """
-                    
-                    writeFile file: BUILD_SETTINGS, text: content
-                }
-            }
+    
+    node('unityci') {
+        if (buildSetting == '(Private)') {
             
+            def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
+            
+            BUILD_SETTINGS = "${destDir}/${privateSite}.json"
+
+            sh "mkdir -p ${destDir}"
+
+            echo destDir
+
+            echo BUILD_SETTINGS
+
+            def content = """
+            {
+                "SkipWebConfig": false,
+                "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
+                "LocalSite": "Private_${privateSite}"
+            }
+            """
+            
+            writeFile file: BUILD_SETTINGS, text: content
         }
-        
     }
 }
