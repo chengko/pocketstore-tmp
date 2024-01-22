71d43a7da23e592455a93d25028efb592c17f4be
cheng <cheng@7senses.com>
2024-01-22 18:31:01 +0800

    try

--- vars/pocketstore.groovy
+++ vars/pocketstore.groovy
@@ -5,26 +5,24 @@ def configure(String projectName, String buildSetting, String privateSite) {
                 label 'unityci' 
             }
         }
-        steps {
-            script {
-                if (buildSetting == '(Private)') {
-                    
-                    def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
-                    
-                    BUILD_SETTINGS = "${destDir}/${privateSite}.json"
+        script {
+            if (buildSetting == '(Private)') {
+                
+                def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
+                
+                BUILD_SETTINGS = "${destDir}/${privateSite}.json"
 
-                    sh "mkdir -p ${destDir}"
+                sh "mkdir -p ${destDir}"
 
-                    def content = """
-                    {
-                      "SkipWebConfig": false,
-                      "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
-                      "LocalSite": "Private_${privateSite}"
-                    }
-                    """
-                    
-                    writeFile file: BUILD_SETTINGS, text: content
+                def content = """
+                {
+                    "SkipWebConfig": false,
+                    "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
+                    "LocalSite": "Private_${privateSite}"
                 }
+                """
+                
+                writeFile file: BUILD_SETTINGS, text: content
             }
         }
     }
