effa14ecadc40d99c1df29bfe0021a4ebf533dcd
cheng <cheng@7senses.com>
2024-01-25 13:45:39 +0800

    1

--- vars/pocketstore.groovy
+++ vars/pocketstore.groovy
@@ -1,32 +1,85 @@
-def configureBuild(String projectName, String buildSetting, String privateSite) {
+def configureSite(buildSetting, privateSite) {
 
-    node('unityci') {
-        def result = [
-            File: "BatchBuildSettings/WebConfig/WebConfig.${buildSetting}.json",
-            Site: buildSetting
-        ]
-        if (buildSetting == '(Private)') {
-            
-            def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/$projectName").toString().trim()
+    def result = [
+        File: "BatchBuildSettings/WebConfig/WebConfig.${buildSetting}.json",
+        Site: buildSetting
+    ]
+    if (buildSetting == '(Private)') {
+        
+        node('unityci') {
+            def destDir = sh(returnStdout: true, script: "echo ~/Documents/config/pocketstore").toString().trim()
             
             result.File = "${destDir}/${privateSite}.json"
             result.Site = privateSite
 
-            sh "mkdir -p ${destDir}"
-
-            echo destDir
-
-            def content = """
-            {
-                "SkipWebConfig": false,
-                "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore-web-config/${privateSite}/IndexFile.json",
-                "LocalSite": "Private_${privateSite}"
-            }
-            """
-            
-            writeFile file: result.File, text: content
+            writeWebConfig(destDir, privateSite)
         }
+        copyArtifacts parameters: "Name=${privateSite}", projectName: 'PocketStore/Generate GlobalSetting', selector: lastSuccessful()
+        def gameCode = sh(script: "jq -r '.GameCode' GlobalSettings.json", returnStdout:true).trim().toInteger()
 
-        return result
+        writeIndexFile(privateSite)
+        writeSite(privateSite, '130.211.246.195:8590', gameCode)
     }
+
+    return result
+}
+
+def writeWebConfig(destDir, site) {
+
+    def content = """
+    {
+        "SkipWebConfig": false,
+        "WebConfigIndexFileUrl": "http://dev.make-wish.club/pocketstore_web_config/${site}/IndexFile.json",
+        "LocalSite": "Private_${site}"
+    }
+    """
+
+    sh "mkdir -p ${destDir}"
+
+    writeFile file: "${destDir}/${site}.json", text: content
+}
+
+def writeIndexFile(site) {
+    def content = """
+    {
+        "BaseUrl": "http://dev.make-wish.club/pocketstore-web-config/",
+        "Files": [
+            "CommonConfig.json",
+            "Announcement.json",
+            "${site}/Site.json"		
+        ]
+    }
+    """
+
+    writeToWeb(site, "IndexFile.json", content)
+}
+
+def writeSite(site, gateway, gameCode) {
+    def content = """
+    {
+        "BundleUrl": "http://13.114.245.218:17080/PS/bundles/GamaniaAWSDaily",
+        "GatewayAddress": [
+            "${gateway}"
+        ],
+        "LoginServiceIds": [
+            ${gameCode + 1}
+        ],
+        "RemoteLogConfig": {
+            "URL": "",
+            "ShowFPS": false,
+            "LogPerformaceInterval": 60.0
+        }
+    }
+    """
+
+    writeToWeb(site, "Site.json", content)
+}
+
+def writeToWeb(site, filename, content) {
+    writeFile file: filename, text: content
+
+    def WEB_CONFIG_ROOT = "/home/ssl-web/pocketstore_web_config"
+
+    sh "docker exec hfs sh -c 'mkdir -p $WEB_CONFIG_ROOT/$site"
+    sh "docker cp ${file} hfs:$WEB_CONFIG_ROOT/$site/"
 }
